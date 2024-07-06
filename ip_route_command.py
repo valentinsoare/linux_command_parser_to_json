9f2f5b900774ac3b1e06b88c5080ecdd6a253773
valentinsoare <soarevalentinn@gmail.com>
2024-07-06 13:51:22 +0300

    adding comments and printing the output code refactored

--- ip_route_command.py
+++ ip_route_command.py
@@ -5,6 +5,11 @@ import textfsm
 
 
 def _take_input() -> str:
+    """
+    Parses command line arguments to get the routing table input as a string.
+    Returns:
+        str: The routing table input provided by the user.
+    """
     parser = argparse.ArgumentParser(description='Parse linux routing table output.')
     parser.add_argument('input_string', type=str, help='Routing table input (ip route output) as a string.')
     args = parser.parse_args()
@@ -13,6 +18,13 @@ def _take_input() -> str:
 
 
 def _take_input_mock() -> str:
+    """
+    Provides a mock input string representing the output of a routing table command.
+    This is useful for testing purposes when actual command line input is not available.
+
+    Returns:
+    str: A mock string representing the output of a routing table command.
+    """
     input_from_user: str = """
 default via 192.168.100.1 dev enp52s0 proto dhcp src 192.168.100.35 metric 100
 172.17.0.0/16 dev docker0 proto kernel scope link src 172.17.0.1
@@ -22,6 +34,16 @@ default via 192.168.100.1 dev enp52s0 proto dhcp src 192.168.100.35 metric 100
 
 
 def _parse_input(name_of_file_template: str, input_from_user: str) -> tuple:
+    """
+    Parses the input string using a TextFSM template to extract routing information.
+
+    Args:
+        name_of_file_template (str): The path to the TextFSM template file.
+        input_from_user (str): The routing table input as a string.
+
+    Returns:
+        tuple: A tuple containing the parsed results and the TextFSM object.
+    """
     with open(name_of_file_template) as template_file:
         fsm = textfsm.TextFSM(template_file)
         parsed_results = fsm.ParseText(input_from_user)
@@ -30,6 +52,17 @@ def _parse_input(name_of_file_template: str, input_from_user: str) -> tuple:
 
 
 def _extract_output(content_parsed: list, fsm_like: object) -> list:
+    """
+    Extracts and formats the parsed routing information into a list of dictionaries.
+
+    Args:
+        content_parsed (list): The parsed routing information as a list of lists.
+        fsm_like (object): The TextFSM object used for parsing.
+
+    Returns:
+        list: A list of dictionaries, each representing a routing entry.
+    """
+
     output: list = ["["]
     elements = fsm_like.header
 
@@ -48,14 +81,21 @@ def _extract_output(content_parsed: list, fsm_like: object) -> list:
 
 
 def _print_the_output(output: list) -> None:
-    for i in range(len(output)):
-        if (i > 0) and (i < (len(output) - 2)):
-            print(output[i], end=",")
-        else:
-            print(output[i], end="")
+    """
+    Prints the extracted routing information.
+    This method formats the output for readability before printing.
+
+    Args:
+        output (list): The extracted routing information as a list of dictionaries.
+    """
+    for i, item in enumerate(output):
+        print(item, end="," if 0 < i < len(output) - 2 else "")
 
 
 def main():
+    """
+    Main function that orchestrates the parsing and printing of routing table information.
+    """
     name_of_file: str = "parser_templates/ip_route.template"
 
     result_from_command_as_a_list_parsed, as_fsm = _parse_input(name_of_file, _take_input_mock())
